a4e10ccf5b68fc16a4ec7cc798d68c876a70ac54
Leo <apollycn@gmail.com>
2025-01-17 01:48:33 -0500

    feat: add text to sidebar

--- modules/dashboard.mjs
+++ modules/dashboard.mjs
@@ -1,15 +1,23 @@
 const ticketsTab = document.querySelector(".tickets-tab");
+const ticketsContainer = document.querySelector(".tickets-container");
+
+const buttonToName = new Map();
+Array.from(ticketsTab.children).forEach((button) => buttonToName.set(button, `${button.classList[0]} ${button.classList[1]}`))
 
 function openTab() {
     Array.from(ticketsTab.children).forEach((button) => {
-        
+        button.insertAdjacentText("afterbegin", buttonToName.get(button));
     });
+    ticketsContainer.style.left = "25vmax";
 }
 
 function closeTab() {
     Array.from(ticketsTab.children).forEach((button) => {
-        
+        button.childNodes.forEach((node) => {
+            if (node.nodeType === Node.TEXT_NODE) node.remove();
+        });
     });
+    ticketsContainer.style.left = "5vmax";
 }
 
 ticketsTab.onclick = () => {
